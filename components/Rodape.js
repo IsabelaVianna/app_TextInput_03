97df247f107f74f3f3a6ab37d0cc55200fd006b8
IsabelaVianna <124220592+IsabelaVianna@users.noreply.github.com>
2026-04-13 17:59:48 -0300

    Update footer

--- components/Rodape.js
+++ components/Rodape.js
@@ -5,7 +5,7 @@ import estilos from '../estilos';
 export default function Rodape() {
   return (
     <View style={estilos.rodapeContainer}>
-      <Text style={estilos.rodapeTexto}>Seu Nome — dd/mm/aaaa</Text>
+      <Text style={estilos.rodapeTexto}>ISABELA VIANNA — 13/04/2026</Text>
     </View>
   );
 }
